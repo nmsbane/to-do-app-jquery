4afd3c281b12caf42299056b92d0b2876d89075d
nmsbane <madhusudhan638@gmail.com>
2016-03-28 18:04:44 +0530

    added the functionality to add new to-do items, learnt the importance of on method instead of click method, also attached event listeners to the elements which are present when the page is loaded with the following syntax see comments on todos.js  this is important otherwise the new to-do items cannot be toggled

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,28 @@
 // when items are clicked give them the class of completed and toggle the class
-$('li').click(function() {
+// when an li is clicked inside an ul
+// but u can't write $('li').on('click')
+// add a listener to the entire ul
+$('ul').on('click', 'li', function() {
 	$(this).toggleClass("completed");
 });
 
 // click on X to delete the To-Do
-$("span").click(function(event) {
+// when span is clicked inside ul
+$("ul").on('click', 'span', function(event) {
 	// remove the parent li item by using fadeout animation
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	// stop the event propagation to enclosing tags
 	event.stopPropagation();
-})
+});
+
+
+// add the new to-do item functionality
+$("input[type='text']").on('keypress', function(event) {
+	if(event.which === 13) {
+		var toDoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> "+ toDoText + "</li>");
+	}
+});
