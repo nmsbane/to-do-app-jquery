adde928bb4373464972781d1bc35fef5e16e4dc2
nmsbane <madhusudhan638@gmail.com>
2016-03-28 17:29:58 +0530

    used jquery to remove the list item when a span tag X was clicked, and also learnt about event propogation

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,13 @@
 $('li').click(function() {
 	$(this).toggleClass("completed");
 });
+
+// click on X to delete the To-Do
+$("span").click(function(event) {
+	// remove the parent li item by using fadeout animation
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	// stop the event propagation to enclosing tags
+	event.stopPropagation();
+})
